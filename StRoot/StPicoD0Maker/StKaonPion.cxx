a7f9bd2a5ce1881eae96fb89cd2c52b442a5378a
Mustafa Mustafa <mstftsm@gmail.com>
2015-03-20 11:22:35 -0700

    add member lorentzVector and remove older variables

--- StRoot/StPicoD0Maker/StKaonPion.cxx
+++ StRoot/StPicoD0Maker/StKaonPion.cxx
@@ -13,26 +13,20 @@
 ClassImp(StKaonPion)
   
 
-
-////////////////////////////////////////////////////////////////////
 StKaonPion::StKaonPion()
 {
   clear();
 }
-////////////////////////////////////////////////////////////////////
-StKaonPion::StKaonPion(StKaonPion* t) {
+//------------------------------------
+StKaonPion::StKaonPion(StKaonPion* t)
+{
   clear();
   mKaonIdx = t->mKaonIdx;
   mPionIdx = t->mPionIdx;
-  mM    = t->mM;
-  mPt = t->mPt;
-  mEta = t->mEta;
-  mPhi = t->mPhi;
-  mOpenAngle = t->mOpenAngle;
+  mLorentzVector = t->mLorentzVector;
   mCosThetaStar = t->mCosThetaStar;
 }
-
-////////////////////////////////////////////////////////////////////
+//------------------------------------
 StKaonPion::StKaonPion(StPicoTrack* kaon, StPicoTrack* pion,unsigned short kIdx,unsigned short pIdx)
 {
   clear();
@@ -40,54 +34,27 @@ StKaonPion::StKaonPion(StPicoTrack* kaon, StPicoTrack* pion,unsigned short kIdx,
 
   StLorentzVectorF kFourMom(kaon->pMom(),kaon->pMom().massHypothesis(.493677));
   StLorentzVectorF pFourMom(pion->pMom(),pion->pMom().massHypothesis(.13957));
-  StLorentzVectorF kpFourMom = kFourMom + pFourMom;
+  mLorentzVector = kFourMom + pFourMom;
 
  
-  StLorentzVectorF kpFourMomReverse(-kpFourMom.px(), -kpFourMom.py(), -kpFourMom.pz(), kpFourMom.e());
+  StLorentzVectorF kpFourMomReverse(-mLorentzVector.px(), -mLorentzVector.py(), -mLorentzVector.pz(), mLorentzVector.e());
   StLorentzVectorF kMomStar = kFourMom.boost(kpFourMomReverse);
-
-  //StThreeVectorF kRotMom(-1.*kaon->pMom().x(),-1.*kaon->pMom().y(),kaon->pMom().z());
-  //StLorentzVectorF kRotFourMom(kRotMom,kRotMom.massHypothesis(.493677));
-  //StLorentzVectorF kpRotFourMom = kRotFourMom + pFourMom;
 
   mKaonIdx = kIdx;
   mPionIdx = pIdx;
 
-  mM = (10000.*kpFourMom.m())>Pico::USHORTMAX ? Pico::USHORTMAX : (UShort_t)(TMath::Nint(10000.*kpFourMom.m()));
-  mPt = (1000.*kpFourMom.perp())>Pico::USHORTMAX ? Pico::USHORTMAX : (UShort_t)(TMath::Nint(1000.*kpFourMom.perp()));
-  mEta = (Short_t)(TMath::Nint(kpFourMom.pseudoRapidity()*1000.));
-  mPhi = (Short_t)(TMath::Nint(kpFourMom.phi()*1000.));
-
-  mOpenAngle = (Short_t)(TMath::Nint(kaon->pMom().angle(pion->pMom()) * 1000.));
-  mCosThetaStar = (Char_t)(TMath::Nint(TMath::Cos(kMomStar.vect().angle(kpFourMom.vect())) * 100. ));
+  mCosThetaStar = static_cast<char>((TMath::Nint(TMath::Cos(kMomStar.vect().angle(mLorentzVector.vect())) * 100. )));
 }
-//////////////////////////
-StLorentzVectorF StKaonPion::lorentzVector()
+//------------------------------------
+StKaonPion::~StKaonPion()
 {
-  Float_t px = pt()*TMath::Cos(phi());
-  Float_t py = pt()*TMath::Sin(phi());
-  Float_t pz = pt()*TMath::SinH(eta());
-  Float_t e = TMath::Sqrt(px*px+py*py+pz*pz+m()*m());
-
-  StLorentzVectorF kpFourMom(px,py,pz,e);
-
-  return kpFourMom;
 }
-////////////////////////////////////////////////////////////////////
-StKaonPion::~StKaonPion() {
-}
-
-////////////////////////////////////////////////////////////////////
-void StKaonPion::clear() {
-
+//------------------------------------
+void StKaonPion::clear()
+{
   mKaonIdx = std::numeric_limits<unsigned short>::max();
   mPionIdx = std::numeric_limits<unsigned short>::max();
 
-  mM=Pico::USHORTMAX;
-  mPt=0;
-  mEta=Pico::SHORTMAX;
-  mPhi=Pico::SHORTMAX;
-  mOpenAngle=Pico::SHORTMAX;
-  mCosThetaStar=0.;
+  mCosThetaStar = std::numeric_limits<char>::min();
 };
 #endif // __ROOT__
--- StRoot/StPicoD0Maker/StKaonPion.h
+++ StRoot/StPicoD0Maker/StKaonPion.h
@@ -17,28 +17,22 @@ class StKaonPion : public TObject {
   ~StKaonPion(); // please keep this non-virtual and NEVER inherit from this class 
   void clear();
 
-  // while ROOT types are not necessary here they are used for consistency with StPicoDst
   unsigned short   kaonIdx() const { return mKaonIdx;}
   unsigned short   pionIdx() const { return mPionIdx;}
-  Float_t m()    const { return (Float_t)mM/10000.;}
-  Float_t pt()   const { return (Float_t)mPt/1000.;}
-  Float_t eta()   const { return (Float_t)mEta/1000.;}
-  Float_t phi()   const { return (Float_t)mPhi/1000.;}
-  Float_t openAngle() const { return (Float_t)mOpenAngle/1000.;}
-  Float_t cosThetaStar() const { return (Float_t)mCosThetaStar/100.;}
-  StLorentzVectorF lorentzVector();
+  float m()    const { return mLorentzVector.m();}
+  float pt()   const { return mLorentzVector.perp();}
+  float eta()   const { return mLorentzVector.pseudoRapidity();}
+  float phi()   const { return mLorentzVector.phi();}
+  float cosThetaStar() const { return static_cast<float>(mCosThetaStar/100.);}
+  StLorentzVectorF lorentzVector() const { return mLorentzVector;}
           
- protected:
+ private:
+  StLorentzVectorF mLorentzVector; // this owns four float only
+
   unsigned short  mKaonIdx; // index of track in StPicoDstEvent
   unsigned short  mPionIdx;
 
-  UShort_t mM; // mass * 10000. : 0.1 MeV precision
-  UShort_t mPt; // pT * 1000.  : MeV precision
-  Short_t mEta; // eta * 1000.
-  Short_t mPhi; // phi * 1000.
-
-  Short_t mOpenAngle; // openAngle * 1000.
-  Char_t mCosThetaStar; // cosThetaStar * 100.
+  char mCosThetaStar; // cosThetaStar * 100.
 
   ClassDef(StKaonPion,1)
 };
