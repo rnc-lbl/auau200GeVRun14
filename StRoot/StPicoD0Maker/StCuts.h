7b04b7a6cfa9743036225ebbbb1145ce2fcf1677
Mustafa Mustafa <mstftsm@gmail.com>
2015-03-21 00:10:41 -0700

    remove unnecessary cuts and update others

--- StRoot/StPicoD0Maker/StCuts.h
+++ StRoot/StPicoD0Maker/StCuts.h
@@ -1,41 +1,25 @@
 #ifndef CUTS_H
 #define CUTS_H
 
-// pay attention that all the pairs invariant mass cuts are applied in the code
 namespace cuts
 {
 	//event
-	const float vz = 100.0;
-	const float vzVpdVz = 6.0;
+	float const vz = 100.0;
+	float const vzVpdVz = 6.0;
 
 	//tracking
-	const int nHitsFit = 15;
-	const float nHitsFitnHitsMax = 0.52;
-	const float dca = 2.0; // this is not applied for global tracks, of course :-)
-
-	// hadrons
-	const float hadronsGlobalPt = 0.2;
-	const float hadronsDca = 3.0;
+	int const nHitsFit = 15;
+	float const nHitsFitnHitsMax = 0.52;
+	float const dca = 2.0;
 
 	//pions 
-	const float nSigmaPion = 3.0;
-	const float pionPt = 0.15; // this is used for soft pions, for other pions 0.2 is used, see code
-	const float pionEta = 1.0; // this is used for all but soft pions
+	float const nSigmaPion = 3.0;
+	float const pionPt = 0.2;
+	float const pionEta = 1.0;
 
 	//kaons 
-	const float kaonPt = 0.2;
-	const float kaonEta = 1.0;
-	const float nSigmaKaon = 3.0;
-
-	// electrons
-	const float electronPt = 0.2;
-	const float electronEta = 1.0;
-	const float nSigmaElectronHigh = 5.0;
-	const float nSigmaElectronLow = -5.0;
-	const float electronsBeta = 0.035; // for tracks with pT< 1.0
-
-	// partner electron cuts
-	const float partnerElecNSigmaElectron = 5.0;
-	const float partnerElecGlobalPt = 0.2;
+	float const nSigmaKaon = 3.0;
+	float const kaonPt = 0.2;
+	float const kaonEta = 1.0;
 }
 #endif
