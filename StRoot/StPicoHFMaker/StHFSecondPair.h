0d3a7d14a07cda1a613c33f40ea0377f9a6656e3
Jochen Thaeder <jochen@thaeder.de>
2015-03-24 14:01:30 -0700

    added secondary pair and fixed mass hypo usage

--- StRoot/StPicoHFMaker/StHFSecondPair.h
+++ StRoot/StPicoHFMaker/StHFSecondPair.h
@@ -1,5 +1,5 @@
-#ifndef StHFPair_hh
-#define StHFPair_hh
+#ifndef StHFSecondPair_hh
+#define StHFSecondPair_hh
 #ifdef __ROOT__
 
 #include "TObject.h"
@@ -8,25 +8,24 @@
 
 class StPicoTrack;
 class StPicoEvent;
-class StHFSecondPair;
 
-class StHFPair : public TObject
+class StHFSecondPair : public TObject
 {
  public:
-  StHFPair();
-  StHFPair(StHFPair const *);
+  StHFSecondPair();
+  StHFSecondPair(StHFSecondPair const *);
 
-  StHFPair(StPicoTrack const * particle1, StPicoTrack const * particle2, 
+  StHFSecondPair(StPicoTrack const * particle1, StPicoTrack const * particle2, 
 	   float p1MassHypo, float p2MassHypo,
 	   unsigned short particle1Idx, unsigned short particle2Idx,
 	   StThreeVectorF const & vtx, float bField);
 
-  StHFPair(StPicoTrack const * particle1, StHFSecondPair const * particle2, 
+  StHFSecondPair(StPicoTrack const * particle1, StHFSecondPair const * particle2, 
 	   float p1MassHypo, float p2MassHypo,
 	   unsigned short particle1Idx, unsigned short particle2Idx,
 	   StThreeVectorF const & vtx, float bField);
 
-  ~StHFPair() {;}
+  ~StHFSecondPair() {;}
 
   StLorentzVectorF const & lorentzVector() const { return mLorentzVector;}
   float m()    const;
@@ -44,8 +43,8 @@ class StHFPair : public TObject
           
  private:
   // disable copy constructor and assignment operator by making them private (once C++11 is available in STAR you can use delete specifier instead)
-  StHFPair(StHFPair const &);
-  StHFPair& operator=(StHFPair const &);
+  StHFSecondPair(StHFSecondPair const &);
+  StHFSecondPair& operator=(StHFSecondPair const &);
   StLorentzVectorF mLorentzVector; // this owns four float only
 
   float mPointingAngle;
@@ -59,21 +58,21 @@ class StHFPair : public TObject
   unsigned short mDcaDaughters; // dcaDaughters * 10000. 1 μm precision
   char           mCosThetaStar; // cosThetaStar * 100.
 
-  ClassDef(StHFPair,1)
+  ClassDef(StHFSecondPair,1)
 };
 
-inline float StHFPair::m()    const { return mLorentzVector.m();}
-inline float StHFPair::pt()   const { return mLorentzVector.perp();}
-inline float StHFPair::eta()  const { return mLorentzVector.pseudoRapidity();}
-inline float StHFPair::phi()  const { return mLorentzVector.phi();}
-inline float StHFPair::pointingAngle() const { return mPointingAngle;}
-inline float StHFPair::decayLength()   const { return mDecayLength;}
-inline float StHFPair::particle1Dca()  const { return mParticle1Dca;}
-inline float StHFPair::particle2Dca()  const { return mParticle2Dca;}
-inline unsigned short   StHFPair::particle1Idx() const { return mParticle1Idx;}
-inline unsigned short   StHFPair::particle2Idx() const { return mParticle2Idx;}
-inline float StHFPair::dcaDaughters() const { return static_cast<float>(mDcaDaughters)/10000.;}
-inline float StHFPair::cosThetaStar() const { return static_cast<float>(mCosThetaStar/100.);}
+inline float StHFSecondPair::m()    const { return mLorentzVector.m();}
+inline float StHFSecondPair::pt()   const { return mLorentzVector.perp();}
+inline float StHFSecondPair::eta()  const { return mLorentzVector.pseudoRapidity();}
+inline float StHFSecondPair::phi()  const { return mLorentzVector.phi();}
+inline float StHFSecondPair::pointingAngle() const { return mPointingAngle;}
+inline float StHFSecondPair::decayLength()   const { return mDecayLength;}
+inline float StHFSecondPair::particle1Dca()  const { return mParticle1Dca;}
+inline float StHFSecondPair::particle2Dca()  const { return mParticle2Dca;}
+inline unsigned short   StHFSecondPair::particle1Idx() const { return mParticle1Idx;}
+inline unsigned short   StHFSecondPair::particle2Idx() const { return mParticle2Idx;}
+inline float StHFSecondPair::dcaDaughters() const { return static_cast<float>(mDcaDaughters)/10000.;}
+inline float StHFSecondPair::cosThetaStar() const { return static_cast<float>(mCosThetaStar/100.);}
 
 #endif
 #endif
