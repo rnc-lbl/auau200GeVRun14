7e3a0c7f2e1d8e483d2e5e07a4078ba4722ab25f
Mustafa Mustafa <mstftsm@gmail.com>
2015-03-24 14:15:41 -0700

    add macro to run StPicoD0AnaMaker

---
+++ StRoot/macros/runPicoD0AnaMaker.C
@@ -0,0 +1,26 @@
+void runPicoD0AnaMaker(TString d0list,TString picolist,TString outFileName)
+{
+	gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
+	loadSharedLibraries();
+
+	gSystem->Load("StPicoDstMaker");
+	gSystem->Load("StPicoD0Maker");
+	gSystem->Load("StPicoD0AnaMaker");
+
+  chain = new StChain();
+	// StPicoDstMaker* picoDstMaker = new StPicoDstMaker(0,inputFile,"picoDstMaker");
+	StPicoD0AnaMaker*  picoD0AnaMaker = new StPicoD0AnaMaker("picoD0AnaMaker",d0list,outFileName.Data());
+
+  chain->Init();
+  int nEntries = picoD0AnaMaker->getEntries();
+  for(int iEvent = 0; iEvent<nEntries; ++iEvent)
+  {
+    chain->Clear();
+    chain->Make();
+    int iret = chain->Make();
+	  if (iret) { cout << "Bad return code!" << iret << endl; break;}
+  }
+
+  chain->Finish();
+  delete chain;
+}
