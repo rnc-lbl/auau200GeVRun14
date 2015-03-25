143ba136e741120abadcc053c81eda96dda11b34
Jochen Thaeder <jochen@thaeder.de>
2015-03-25 09:34:41 -0700

    updated version of cut class

--- StRoot/StPicoHFMaker/StHFCuts.h
+++ StRoot/StPicoHFMaker/StHFCuts.h
@@ -1,33 +1,173 @@
-#ifndef HFCUTS_H
-#define HFCUTS_H
+#ifndef StHFCUTS_H
+#define StHFCUTS_H
+#ifdef __ROOT__
 
-namespace hfcuts
+/* **************************************************
+ *  Cut class for HF analysis
+ *  - used as a singlton
+ *
+ *  Authors:
+ *
+ * **************************************************
+ */
+
+#include "TObject.h"
+
+class StPicoTrack;
+
+class StHFPair;
+class StHFSecondaryPair;
+class StHFTriplet;
+
+class StHFCuts : public TObject
 {
-   //event
-   float const vz = 100.0;
-   float const vzVpdVz = 6.0;
+ public:
+  
+  static StHFCuts* Instance();
+  
+  ~StHFCuts() {;}
+  
+  // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
-   //tracking
-   int   const nHitsFit = 20;
-   bool  const requireHFT = true;
-   float const nHitsFitnHitsMax = 0.52; // applied in StPicoDstMaker for Run14 data, no need to reapply here
+  void SetCutVzMax(float f)            { mVzMax = f; }
+  void SetCutVzVpdVzMax(float f)       { mVzVpdVzMax = f; }
 
-   //pions
-   float const nSigmaPion = 3.0;
-   float const pionPt = 0.2;
+  
 
-   //kaons
-   float const nSigmaKaon = 2.0;
-   float const kaonPt = 0.2;
 
-   //protons
-   float const nSigmaKaon = 2.0;
-   float const kaonPt     = 0.2;
+  void SetCutNHitsFitMax(int i)        { mNHitsFitMax = i; }
+  void SetCutRequireHFT(bool b)        { mRequireHFT = b; }
+  void SetCutNHitsFitnHitsMax(float f) { mNHitsFitnHitsMax = f; }
 
-   // kaonPion pair cuts
-   float const dcaDaughters = 0.0200; // maximum
-   float const decayLength = 0.0030; // minimum
-   float const minMass = 1.6;
-   float const maxMass = 2.1;
-}
+  void SetCutNSigmaPion(float f)       { mNSigmaPion = f; }
+  void SetCutPionPt(float f)           { mPionPt = f; }
+  void SetCutPionEta(float f)          { mPionEta = f; }
+  
+  void SetCutNSigmaKaon(float f)       { mNSigmaKaon = f; }
+  void SetCutKaonPt(float f)           { mKaonPt = f; }
+  void SetCutKaonEta(float f)          { mKaonEta = f; }
+
+  void SetCutNSigmaProton(float f)     { mNSigmaProton = f; }
+  void SetCutProtonPt(float f)         { mProtonPt = f; }
+  void SetCutProtonEta(float f)        { mProtonEta = f; }
+
+  void SetCutPrimPair(float dcaDaughtersMax, float decayLengthMin, float massMin, float massMax)  {
+    mPrimDcaDaughtersMax = dcaDaughtersMax; mPrimDecayLengthMin = decayLengthMin;
+    mPrimMassMin = massMin; mPrimMassMax = massMax; }
+
+  void SetCutSecondaryPair(float dcaDaughtersMax, float decayLengthMin, float massMin, float massMax)  {
+    mSecondDcaDaughtersMax = dcaDaughtersMax; mSecondDecayLengthMin = decayLengthMin;
+    mSecondMassMin = massMin; mSecondMassMax = massMax; }
+
+  // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+  
+  const float& CutVzMax()                 const { return mVzMax; }
+  const float& CutVzVpdVzMax()            const { return mVzVpdVzMax; }
+
+  const int& CutNHitsFitMax()             const { return mNHitsFitMax; }
+  const bool& CutRequireHFT()             const { return mRequireHFT; }
+  const float& CutNHitsFitnHitsMax()      const { return mNHitsFitnHitsMax; }
+
+  const float& CutNSigmaPion()            const { return mNSigmaPion; }
+  const float& CutPionPt()                const { return mPionPt; }
+  const float& CutPionEta()               const { return mPionEta; }
+  
+  const float& CutNSigmaKaon()            const { return mNSigmaKaon; }
+  const float& CutKaonPt()                const { return mKaonPt; }
+  const float& CutKaonEta()               const { return mKaonEta; }
+
+  const float& CutNSigmaProton()          const { return mNSigmaProton; }
+  const float& CutProtonPt()              const { return mProtonPt; }
+  const float& CutProtonEta()             const { return mProtonEta; }
+
+  const float& CutPrimDcaDaughtersMax()   const { return mPrimDcaDaughtersMax; }
+  const float& CutPrimDecayLengthMin()    const { return mPrimDecayLengthMin; }
+  const float& CutPrimMassMin()           const { return mPrimMassMin; }
+  const float& CutPrimMassMax()           const { return mPrimMassMax; }
+
+  const float& CutSecondDcaDaughtersMax() const { return mSecondDcaDaughtersMax; }
+  const float& CutSecondDecayLengthMin()  const { return mSecondDecayLengthMin; }
+  const float& CutSecondMassMin()         const { return mSecondMassMin; }
+  const float& CutSecondMassMax()         const { return mSecondMassMax; }
+
+  const float& MassHypothesisParticle1()        const { return mMassHypo[0]; }
+  const float& MassHypothesisParticle2()        const { return mMassHypo[1]; }
+  const float& MassHypothesisParticle3()        const { return mMassHypo[2]; }
+
+  // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --   
+
+  bool IsGoodEvent();
+
+  bool IsGoodTrack(StPicoTrack const *trk);
+
+
+
+  //  bool IsPionTPC(StPicoTrack const *trk)
+  // bool IsPionTOF(StPicoTrack const *trk, float const & bTofBeta)
+  
+
+  bool IsGoodPrimaryPair(StHFPair const & pair) const;
+  bool IsGoodSecondaryPair(StHFSecondaryPair const & pair) const;
+  bool IsGoodTriplet(StHFTriplet const & triplet) const;
+
+  // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --   
+
+ private:
+  
+  StHFCuts();  
+  StHFCuts(StHFCuts const &);       
+  StHFCuts& operator=(StHFCuts const &); 
+
+  static StHFCuts* mStHFCuts;
+
+  // -- event cuts
+  float mVzMax;
+  float mVzVpdVzMax;
+
+  // -- tracking
+  int   mNHitsFitMax;
+  bool  mRequireHFT;
+  float mNHitsFitnHitsMax; // applied in StPicoDstMaker for Run14 data, no need to reapply here
+  
+  float mMassHypo[3];
+
+  // ------------------------------------------
+  // -- Basic cuts for particles
+  //    too fill particle vectors
+  // ------------------------------------------
+  // -- pions
+  float mNSigmaPion;
+  float mPionPt;
+  float mPionEta;
+  
+  // -- kaons
+  float mNSigmaKaon;
+  float mKaonPt;
+  float mKaonEta;
+
+  // -- protons
+  float mNSigmaProton;
+  float mProtonPt;
+  float mProtonEta;
+
+  // ------------------------------------------
+  // -- Pair cuts for primary pair
+  // ------------------------------------------
+  float mPrimDcaDaughtersMax;
+  float mPrimDecayLengthMin; 
+  float mPrimMassMin;
+  float mPrimMassMax;
+
+  // ------------------------------------------
+  // -- Pair cuts for secondary pair
+  // ------------------------------------------
+  float mSecondDcaDaughtersMax;
+  float mSecondDecayLengthMin; 
+  float mSecondMassMin;
+  float mSecondMassMax;
+
+  ClassDef(StHFCuts,1)
+};
+
+#endif
 #endif
