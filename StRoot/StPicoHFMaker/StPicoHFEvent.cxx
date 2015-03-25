63207e6f6f70326cc1d904d1a10d69347d9451c7
Jochen Thaeder <jochen@thaeder.de>
2015-03-25 10:24:54 -0700

    compiling version ...

--- StRoot/StPicoHFMaker/StPicoHFEvent.cxx
+++ StRoot/StPicoHFMaker/StPicoHFEvent.cxx
@@ -2,38 +2,82 @@
 #include "StPicoDstMaker/StPicoTrack.h"
 
 #include "StPicoHFEvent.h"
-#include "StKaonPion.h"
+#include "StHFPair.h"
+#include "StHFSecondaryPair.h"
+#include "StHFTriplet.h"
 
 ClassImp(StPicoHFEvent)
 
-TClonesArray *StPicoHFEvent::fgKaonPionArray = 0;
+TClonesArray *StPicoHFEvent::fgHFPrimaryArray = 0;
+TClonesArray *StPicoHFEvent::fgHFSecondaryArray = 0;
 
 //-----------------------------------------------------------------------
-StPicoHFEvent::StPicoHFEvent() : mRunId(-1), mEventId(-1), mNKaonPion(0), mKaonPionArray(NULL)
+StPicoHFEvent::StPicoHFEvent() : mRunId(-1), mEventId(-1), mNHFPrimary(0), mNHFSecondary(0), 
+				 mHFPrimaryArray(NULL), mHFSecondaryArray(NULL) 
 {
-   if (!fgKaonPionArray) fgKaonPionArray = new TClonesArray("StKaonPion");
-   mKaonPionArray = fgKaonPionArray;
+  // -- Default constructor
+
+   if (!fgHFPrimaryArray) fgHFPrimaryArray = new TClonesArray("StHFPair");
+   mHFPrimaryArray = fgHFPrimaryArray;
+}
+
+//-----------------------------------------------------------------------
+StPicoHFEvent::StPicoHFEvent(unsigned int mode) : mRunId(-1), mEventId(-1), mNHFPrimary(0), mNHFSecondary(0), 
+						  mHFPrimaryArray(NULL), mHFSecondaryArray(NULL) 
+{
+  // -- Constructor with mode selection
+  if (mode == StPicoHFEvent::secondPair) {
+    if (!fgHFPrimaryArray) fgHFPrimaryArray = new TClonesArray("StHFPair");
+    mHFPrimaryArray = fgHFPrimaryArray;
+
+    if (!fgHFSecondaryArray) fgHFSecondaryArray = new TClonesArray("StHFSecondaryPair");
+    mHFSecondaryArray = fgHFSecondaryArray;
+  }
+  else if (mode == StPicoHFEvent::triplet) {
+    if (!fgHFPrimaryArray) fgHFPrimaryArray = new TClonesArray("StHFTriplet");
+    mHFPrimaryArray = fgHFPrimaryArray;
+  }
+  else {
+    if (!fgHFPrimaryArray) fgHFPrimaryArray = new TClonesArray("StHFPair");
+    mHFPrimaryArray = fgHFPrimaryArray;
+  }
 }
 
 //-----------------------------------------------------------------------
 void StPicoHFEvent::addPicoEvent(StPicoEvent const & picoEvent)
 {
    // StPicoEvent variables
-   mRunId = picoEvent.runId();
+   mRunId   = picoEvent.runId();
    mEventId = picoEvent.eventId();
 }
 
 //-----------------------------------------------------------------------
 void StPicoHFEvent::clear(char const *option)
 {
-   mKaonPionArray->Clear(option);
-   mRunId = -1;
+   mHFPrimaryArray->Clear(option);
+   if (mHFSecondaryArray)
+     mHFSecondaryArray->Clear(option);
+
+   mRunId   = -1;
    mEventId = -1;
-   mNKaonPion = 0;
+   mNHFPrimary   = 0;
+   mNHFSecondary = 0;
 }
+
 //---------------------------------------------------------------------
-void StPicoHFEvent::addKaonPion(StKaonPion const* t)
-{
-   TClonesArray &kaonPionArray = *mKaonPionArray;
-   new(kaonPionArray[mNKaonPion++]) StKaonPion(t);
+void StPicoHFEvent::addHFPrimary(StHFPair const* t) {
+   TClonesArray &primaryArray = *mHFPrimaryArray;
+   new(primaryArray[mNHFPrimary++]) StHFPair(t);
+}
+
+//---------------------------------------------------------------------
+void StPicoHFEvent::addHFPrimary(StHFTriplet const* t) {
+   TClonesArray &primaryArray = *mHFPrimaryArray;
+   new(primaryArray[mNHFPrimary++]) StHFTriplet(t);
+}
+
+//---------------------------------------------------------------------
+void StPicoHFEvent::addHFSecondary(StHFSecondaryPair const* t) {
+   TClonesArray &secondaryArray = *mHFSecondaryArray;
+   new(secondaryArray[mNHFSecondary++]) StHFSecondaryPair(t);
 }
