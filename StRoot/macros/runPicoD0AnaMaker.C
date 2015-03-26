e667c173081b5ad658800e3d620ea6fa809f623f
Mustafa Mustafa <mstftsm@gmail.com>
2015-03-26 08:54:03 -0700

    correct name of picoDst files

--- StRoot/macros/runPicoD0AnaMaker.C
+++ StRoot/macros/runPicoD0AnaMaker.C
@@ -12,6 +12,8 @@ void runPicoD0AnaMaker(TString d0list,TString outFileName)
   // create list of picoDst files
   TString command = "sed 's/hft\\\/d0tree/picodsts/g' "+d0list+" >correspondingPico.list";
   gSystem->Exec(command.Data());
+  command = "sed -i 's/picoD0/picoDst/g' correspondingPico.list";
+  gSystem->Exec(command.Data());
 	StPicoDstMaker* picoDstMaker = new StPicoDstMaker(0,"correspondingPico.list","picoDstMaker");
 	StPicoD0AnaMaker*  picoD0AnaMaker = new StPicoD0AnaMaker("picoD0AnaMaker",d0list,outFileName.Data(),picoDstMaker);
   
