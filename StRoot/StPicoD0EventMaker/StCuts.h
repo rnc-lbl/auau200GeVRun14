dad79162018eb98d366a158cada63d6d2770ec3e
Mustafa Mustafa <mstftsm@gmail.com>
2015-03-28 07:43:05 -0700

    fix Lomnitz email address

--- StRoot/StPicoD0EventMaker/StCuts.h
+++ StRoot/StPicoD0EventMaker/StCuts.h
@@ -5,7 +5,7 @@
  *  Cuts namespace.
  *
  *  Authors:  Xin Dong        (xdong@lbl.gov)
- *            Michael Lomnitz (mlomitz@lbl.gov)
+ *            Michael Lomnitz (mrlomnitz@lbl.gov)
  *            Mustafa Mustafa (mmustafa@lbl.gov)
  *            Jochen Thaeder  (jmthader@lbl.gov)   
  *
