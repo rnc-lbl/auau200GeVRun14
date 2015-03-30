b28a2c03d87d1cba5b89bcf94a8c0df2831adda1
Mustafa Mustafa <mstftsm@gmail.com>
2015-03-29 22:29:04 -0700

    remove unnecessary imports and clean out the commented codes

--- picoProductionMonitor/syncAndPlot.py
+++ picoProductionMonitor/syncAndPlot.py
@@ -1,10 +1,6 @@
 #!/usr/common/usg/software/python/2.7.6/bin/python
 __author__ = "Mustafa Mustafa"
 __email__ = "mmustafa@lbl.gov"
-import sys
-import os
-import fcntl
-import time
 import subprocess
 
 # gWaitTime = 60*120 # 120 minutes before trying to submit again
@@ -15,14 +11,9 @@ gDirectoryName = './CheckLog'
 
 def main():
 
-    # while True:
-        directoryToSync = gRcf+':'+gLogsLocationAtRcf
-        subprocess.call(('rsync','-avzhp',directoryToSync,gLogsLocationAtPdsf))
-        subprocess.call(('./plotPicoProductionStatistics.py',gDirectoryName))
-
-        # os.system("date")
-        # print "... waiting for another ",gWaitTime/60," minutes before trying to submit again"
-        # time.sleep(gWaitTime)
+    directoryToSync = gRcf+':'+gLogsLocationAtRcf
+    subprocess.call(('rsync','-avzhp',directoryToSync,gLogsLocationAtPdsf))
+    subprocess.call(('./plotPicoProductionStatistics.py',gDirectoryName))
 
 
 if __name__ == '__main__':
