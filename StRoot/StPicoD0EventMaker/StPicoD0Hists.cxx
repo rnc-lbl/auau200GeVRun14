f3d26a8219eaef13d029ececf14cee5559ad0865
Mustafa Mustafa <mstftsm@gmail.com>
2015-04-01 06:30:40 -0700

    fill histograms

--- StRoot/StPicoD0EventMaker/StPicoD0Hists.cxx
+++ StRoot/StPicoD0EventMaker/StPicoD0Hists.cxx
@@ -2,7 +2,7 @@
 #include "TH2F.h"
 #include "TFile.h"
 #include "TString.h"
-#include "../StPicoDstMaker/StPicoTrack.h"
+#include "../StPicoDstMaker/StPicoEvent.h"
 #include "../StPicoPrescales/StPicoPrescales.h"
 #include "StPicoD0Event.h"
 #include "StKaonPion.h"
@@ -26,10 +26,10 @@ StPicoD0Hists::StPicoD0Hists(TString fileBaseName) : mPrescales(NULL), mOutFile(
   mh1TotalEventsInRun = new TH1F("mh1TotalEventsInRun","totalEventsInRun;runIndex",nRuns+1,0,nRuns+1);
   mh1TotalHftTracksInRun = new TH1F("mh1TotalHftTracksInRun","totalHftTracksInRun;runIndex;totalHftTracksInRun",nRuns+1,0,nRuns+1);
   mh1TotalGRefMultInRun = new TH1F("mh1TotalGRefMultInRun","totalGRefMultInRun;runIndex;totalGRefMultInRun",nRuns+1,0,nRuns+1);
-  mh1TotalKaonsInRun = new TH1F("mh1TotalKaonsInRun","totalKaonsInRun;runIndex",nRuns+1,0,nRuns+1);
-  mh1TotalPionsInRun = new TH1F("mh1TotalPionsInRun","totalPionsInRun;runIndex",nRuns+1,0,nRuns+1);
-  mh1TotalD0CandidatesInRun = new TH1F("mh1TotalD0CandidatesInRun","totalD0CandidatesInRun;runIndex",nRuns+1,0,nRuns+1);
-  mh2NKaonsVsNPions = new TH2F("mh2NKaonsVsNPions","nKaonsVsNPions;nKaons;nPions",300,0,300,1000,0,1000);
+  mh1TotalKaonsInRun = new TH1F("mh1TotalKaonsInRun","totalKaonsInRun;runIndex;totalKaonsInRun",nRuns+1,0,nRuns+1);
+  mh1TotalPionsInRun = new TH1F("mh1TotalPionsInRun","totalPionsInRun;runIndex;totalPionsInRun",nRuns+1,0,nRuns+1);
+  mh1TotalD0CandidatesInRun = new TH1F("mh1TotalD0CandidatesInRun","totalD0CandidatesInRun;runIndex:totalD0CandidatesInRun",nRuns+1,0,nRuns+1);
+  mh2NKaonsVsNPions = new TH2F("mh2NKaonsVsNPions","nKaonsVsNPions;nPions;nKaons",1000,0,1000,300,0,300);
   mh2KaonDcaVsPt = new TH2F("mh2KaonDcaVsPt","kaonDcaVsPt;p_{T}(k#pi)(GeV/c);dca(cm)",120,0,12,50,0,0.05);
   mh2PionDcaVsPt = new TH2F("mh2PionDcaVsPt","pionDcaVsPt;p_{T}(k#pi)(GeV/c);dca(cm)",120,0,12,50,0,0.05);
   mh2InvariantMassVsPt = new TH2F("mh2InvariantMassVsPt","invariantMassVsPt;p_{T}(K#pi)(GeV/c);m_{K#pi}(GeV/c^{2})",120,0,12,50,1.6,2.1);
@@ -38,17 +38,26 @@ StPicoD0Hists::~StPicoD0Hists()
 {
   delete mPrescales;
   // note that histograms are owned by mOutFile. They will be destructed 
-  // when the file if closed.
+  // when the file is closed.
 }
 //-----------------------------------------------------------------------
-void StPicoD0Hists::addEvent(StPicoD0Event const & picoD0Event)
+void StPicoD0Hists::addEvent(StPicoEvent const& picoEvent,StPicoD0Event const & picoD0Even,unsigned int const nHftTracks)
 {
   int runIndex = mPrescales->runIndex(picoD0Event.runId());
   mh1TotalEventsInRun->Fill(runIndex);
+  mh1TotalEventsInRun->Fill(runIndex,nHftTracks);
+  mh1TotalRefMultInRun->Fill(runIndex,picoEvent.grefMult());
+  mh1TotalKaonsInRun->Fill(runIndex,picoD0Event.nKaons());
+  mh1TotalPionsInRun->Fill(runIndex,picoD0Event.nPions());
+  mh1TotalD0CandidatesInRun->Fill(runIndex,picoD0Event.nKaonPion());
+  mh2NKaonsVsNPions->Fill(picoD0Event.nPions(),picoD0Event.nKaons());
 }
 //---------------------------------------------------------------------
-void StPicoD0Hists::addKaonPion(StKaonPion const* const kp, StPicoTrack const* const kaon, StPicoTrack const* const pion)
+void StPicoD0Hists::addKaonPion(StKaonPion const* const kp, bool const fillMass)
 {
+  mh2KaonDcaVsPt->Fill(kp->pt(),kp->kaonDca());
+  mh2PionDcaVsPt->Fill(kp->pt(),kp->pionDca());
+  if(fillMass) mh2InvariantMassVsPt->Fill(kp->pt(),kp->m());
 }
 //---------------------------------------------------------------------
 void StPicoD0Hists::closeFile()
