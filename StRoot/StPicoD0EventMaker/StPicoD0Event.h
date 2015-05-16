bf0248b59ba07fe391fff0552419b150a31e8c42
Mustafa Mustafa <mstftsm@gmail.com>
2015-05-16 11:50:27 -0700

    modify authors lists as per our new organizational scheme

--- StRoot/StPicoD0EventMaker/StPicoD0Event.h
+++ StRoot/StPicoD0EventMaker/StPicoD0Event.h
@@ -6,9 +6,9 @@
  *  candidates. 
  *
  *  Authors:  Xin Dong        (xdong@lbl.gov)
- *            Michael Lomnitz (mrlomnitz@lbl.gov)
- *            Mustafa Mustafa (mmustafa@lbl.gov)
- *            Jochen Thaeder  (jmthader@lbl.gov)   
+ *            **Mustafa Mustafa (mmustafa@lbl.gov)
+ *
+ *  **Code Maintainer
  *
  * **************************************************
  */
