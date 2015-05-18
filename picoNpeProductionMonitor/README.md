98ea6ce2fcd27b6dd18ab19b8b4995cc439d6253
kunsuoh <kunsuoh@gmail.com>
2015-05-18 11:32:27 +0900

    remove web page
    we should post QA plots at protected area.

--- picoNpeProductionMonitor/README.md
+++ picoNpeProductionMonitor/README.md
@@ -1,3 +1,2 @@
-http://portal.nersc.gov/project/star/kunsu/run14/picoNpeProductionMonitor/
 Requirement : pandoc, root
 How to use : ./createHtml.sh ROOT_FILE_NAME
--- picoNpeProductionMonitor/createHtml.sh
+++ picoNpeProductionMonitor/createHtml.sh
@@ -7,7 +7,7 @@ elif [ -e $1 ]; then
 	root -l -b -q plotPicoNpeProductionQa.C\(\"$1\"\) 
 	pandoc --standalone --mathjax --from markdown --to html -o index.html index.temp.md
 	rm index.temp.md
-	scp *.png index.html pdsf.nersc.gov:~/www/run14/picoNpeProductionMonitor/
+#	scp *.png index.html pdsf.nersc.gov:~/www/run14/picoNpeProductionMonitor/
 else 
 	echo "Wrong input file."
 fi
