218774ff7df8a3e41ae4400b3a5ccf733d90c356
kunsuoh <kunsuoh@gmail.com>
2015-05-26 21:26:49 +0900

    SetDebug level 5

--- StRoot/macros/runPicoNpeAnaMaker.C
+++ StRoot/macros/runPicoNpeAnaMaker.C
@@ -44,7 +44,7 @@ void runPicoNpeAnaMaker(TString npeList, TString outFileName, TString badRunList
     // -------------- USER variables -------------------------
     
     // add your cuts here.
-    chain->SetDebug(1); 
+    chain->SetDebug(5);
     chain->Init();
     cout << "DEBUG!" << endl;
     int nEntries = picoNpeAnaMaker->getEntries();
