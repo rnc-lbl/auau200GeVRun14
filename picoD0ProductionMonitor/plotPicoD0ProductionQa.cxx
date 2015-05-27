bdaa413556f68609f1179d7a5c106e503bb40973
Mustafa Mustafa <mstftsm@gmail.com>
2015-05-27 15:50:51 -0700

    add histograms for pT bin slices

--- picoD0ProductionMonitor/plotPicoD0ProductionQa.cxx
+++ picoD0ProductionMonitor/plotPicoD0ProductionQa.cxx
@@ -9,10 +9,12 @@
 #ifndef __CINT__
 #include <iostream>
 #include <string>
+#include <utility>
 
 #include "TFile.h"
 
 #include "TF1.h"
+#include "TH1.h"
 #include "TH1F.h"
 #include "TH2F.h"
 #include "TStyle.h"
@@ -20,13 +22,17 @@
 #include "TGaxis.h"
 #include "TColor.h"
 #include "Rtypes.h"
+#include "TMath.h"
+#include "TLatex.h"
 #endif
 
 using namespace std;
 
 void setGraphicsStyle();
 void drawQaHistograms(string filename);
-void setStyle(TH1F* h,Color_t, string ytitle);
+void setStyle(TH1* h,Color_t, string ytitle);
+void drawD0InPtBins(TCanvas*, TH2F*,int nEvents);
+std::pair<float,float> getSignificance(TF1* f,float integrationSigma,float binWidth);
 
 int main(int argc,char** argv)
 {
@@ -78,17 +84,75 @@ void drawQaHistograms(string filename)
   h2PionDcaVsPt->Draw("colz"); cv->SaveAs("h2PionDcaVsPt.png");
   h2CosThetaVsPt->Draw("colz"); cv->SaveAs("h2CosThetaVsPt.png");
   h2DcaDaughtersVsPt->Draw("colz"); cv->SaveAs("h2DcaDaughtersVsPt.png");
+  drawD0InPtBins(cv,h2InvariantMassVsPt,h1TotalEventsInRun->GetEntries()/1e6);
   delete cv;
 
   fInput->Close();
 }
 
-void setStyle(TH1F* h,Color_t color,string title)
+void drawD0InPtBins(TCanvas* cv,TH2F* h,int nEvents)
+{
+  float ptMin[] = {1.5,1.5,2.5,3.5,5.0,07.0};
+  float ptMax[] = {12.,2.5,3.5,5.0,7.0,12.0};
+  size_t nPt = sizeof(ptMin)/sizeof(ptMin[0]);
+
+  for(size_t iPt=0; iPt < nPt; ++iPt)
+  {
+    h->GetXaxis()->SetRangeUser(ptMin[iPt],ptMax[iPt]);
+    cv->cd();
+    TH1* hProj = (TH1*)h->ProjectionY();
+    setStyle(hProj,1,"Counts (per 10 MeV/c^{2})");
+    hProj->GetXaxis()->SetRangeUser(1.665,2.1);
+    hProj->SetMinimum(0);
+    hProj->Draw();
+    TF1 fun("fun","gaus+expo(3)",1.66,2.1);
+    fun.SetParameters(30,1.865,0.015,0,0);
+    fun.SetParLimits(2,0.008,0.030);
+    fun.SetLineWidth(4);
+    fun.SetLineColor(2);
+    fun.SetLineStyle(2);
+    hProj->Fit(&fun,"RN");
+    fun.Draw("same");
+    hProj->Draw("same");
+    auto significance = getSignificance(&fun,2.5,hProj->GetBinWidth(1));
+    TLatex latex;
+    latex.SetTextFont(132);
+    latex.DrawLatexNDC(0.60,0.87,"Au+Au #sqrt{s_{NN}} = 200 GeV");
+    latex.DrawLatexNDC(0.60,0.81,"RHIC Run 2014");
+    latex.DrawLatexNDC(0.60,0.75,Form("%1.2f < p_{T} < %1.2f",ptMin[iPt],ptMax[iPt]));
+    latex.DrawLatexNDC(0.15,0.33,Form("%iM MinBias Events",nEvents));
+    latex.DrawLatexNDC(0.15,0.26,Form("S/#sqrt{S+B} = %i",TMath::Nint(significance.second)));
+    latex.DrawLatexNDC(0.15,0.19,Form("Counts = %i",TMath::Nint(significance.first)));
+
+    cv->SaveAs(Form("%s_pt%i.png",h->GetName(),static_cast<int>(iPt)));
+  }
+}
+
+std::pair<float,float> getSignificance(TF1* f,float integrationSigma,float binWidth)
+{
+  float A = f->GetParameter(0);
+  float mean = f->GetParameter(1);
+  float sigma = f->GetParameter(2);
+  TF1 fGaus("fGaus","gaus",f->GetXmin(),f->GetXmax());
+  fGaus.SetParameters(A,mean,sigma);
+
+  float signal = fGaus.Integral(mean-integrationSigma*sigma,mean+integrationSigma*sigma)/binWidth;
+  float all = f->Integral(mean-integrationSigma*sigma,mean+integrationSigma*sigma)/binWidth;
+
+  return {signal,signal/sqrt(all)};
+}
+
+void setStyle(TH1* h,Color_t color,string title)
 {
   h->GetYaxis()->SetTitle(title.c_str());
   h->SetMarkerStyle(20);
   h->SetLineColor(color);
   h->SetMarkerColor(color);
+  h->SetTitleFont(132,"X");
+  h->SetLabelFont(132,"X");
+  h->SetTitleFont(132,"Y");
+  h->SetLabelFont(132,"Y");
+  h->SetMarkerSize(1.4);
 }
 
 void setGraphicsStyle()
@@ -126,13 +190,7 @@ void setGraphicsStyle()
     gStyle->SetStatX(0.87);
     gStyle->SetStatY(0.87);
 
-    const Int_t NRGBs = 5;
     const Int_t NCont = 255;
-    Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
-    Double_t reds[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
-    Double_t greens[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
-    Double_t blues[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
-    Int_t  FI = TColor::CreateGradientColorTable(NRGBs, stops, reds, greens, blues, NCont);
     gStyle->SetNumberContours(NCont);
     gStyle->UseCurrentStyle();
     // **************************************************************************************
