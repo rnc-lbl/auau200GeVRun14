c149cd559b0f1d5c0936a68ae8f89f44e4e0bd35
Mustafa Mustafa <mstftsm@gmail.com>
2015-05-27 16:18:40 -0700

    add high resolution colors

--- picoD0ProductionMonitor/plotPicoD0ProductionQa.cxx
+++ picoD0ProductionMonitor/plotPicoD0ProductionQa.cxx
@@ -190,9 +190,13 @@ void setGraphicsStyle()
     gStyle->SetStatX(0.87);
     gStyle->SetStatY(0.87);
 
+    const Int_t NRGBs = 5;
     const Int_t NCont = 255;
+    Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+    Double_t reds[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+    Double_t greens[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+    Double_t blues[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+    TColor::CreateGradientColorTable(NRGBs, stops, reds, greens, blues, NCont);
     gStyle->SetNumberContours(NCont);
     gStyle->UseCurrentStyle();
-    // **************************************************************************************
 }
-
