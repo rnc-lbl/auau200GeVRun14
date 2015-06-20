5aa3a9d94b26d63ec063940faa3f28dce63dbcc6
Michael R Lomnitz <mlomnitz@kent.edu>
2015-06-19 17:06:08 -0700

    Add pt dependent cuts

--- StRoot/StPicoMixedEventMaker/StMixerCuts.h
+++ StRoot/StPicoMixedEventMaker/StMixerCuts.h
@@ -43,11 +43,24 @@ namespace mxeCuts
   float const kTofBetaDiff = 0.03;
   
   //Topology
-  float const cosTheta = 0.995;
-  float const dcaDaughters = 0.005;
-  float const massMin = 1.6;
-  float const massMax = 2.1;  
-  float const decayMin = 0.003;
-  float const decayMax = 9999.;
+  float const massMin = 0;
+  float const massMax = 2.5;
+  int const massBins = 250;
+  int   const nPtBins = 5;
+  float const PtEdge[nPtBins+1] = {0., 1., 2., 3., 5., 10.};
+  //Ultimate1
+  //float const dcaV0ToPv[nPtBins] = {0.0062, 0.0047, 0.0040, 0.0041, 0.0042};
+  //float const decayLength[nPtBins] = {0.0149, 0.0205, 0.0216, 0.0233, 0.0282};
+  //float const cosTheta[nPtBins] = {0.0000, 0.0000, 0.0000, 0.0000, 0.0000};//0.995
+  //float const dcaDaughters[nPtBins] = {0.0082, 0.0070, 0.0056, 0.0065, 0.0065}; //0.0050;
+  //float const kDca[nPtBins] = {0.0123, 0.0097, 0.0091, 0.0075, 0.0053};//0.008, // minimum
+  //float const pDca[nPtBins] = {0.0109, 0.0108, 0.0100, 0.0074, 0.0067};//0.008
+  //Ultimate 2
+  float const dcaV0ToPv[nPtBins] = {0.0061, 0.0049, 0.0038, 0.0038, 0.0040};
+  float const decayLength[nPtBins] = {0.0145, 0.0181, 0.0212, 0.0247, 0.0259};
+  float const cosTheta[nPtBins] = {0.0000, 0.0000, 0.0000, 0.0000, 0.0000};//0.995
+  float const dcaDaughters[nPtBins] = {0.0084, 0.0066, 0.0057, 0.0050, 0.0060}; //0.0050;
+  float const kDca[nPtBins] = {0.0103, 0.0091, 0.0095, 0.0079, 0.0058};//0.008, // minimum
+  float const pDca[nPtBins] = {0.0110, 0.0111, 0.0086, 0.0081, 0.0062};//0.008
 }
 #endif
