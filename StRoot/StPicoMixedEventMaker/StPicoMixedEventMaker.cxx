6fb6663d4cd48b19b65f3f1e180cb2fd3230b325
Michael R Lomnitz <mlomnitz@kent.edu>
2015-07-08 13:00:53 -0700

    Fix bug by protecting agains centralities outside array range

--- StRoot/StPicoMixedEventMaker/StPicoMixedEventMaker.cxx
+++ StRoot/StPicoMixedEventMaker/StPicoMixedEventMaker.cxx
@@ -131,6 +131,7 @@ Int_t StPicoMixedEventMaker::Make() {
     mGRefMultCorrUtil->init(picoDst->event()->runId());
     mGRefMultCorrUtil->initEvent(picoDst->event()->grefMult(),pVtx.z(),picoDst->event()->ZDCx()) ;
     int const centrality  = mGRefMultCorrUtil->getCentralityBin9();
+    if(centrality < 0 || centrality >8 ) return kStOk;
     int const vz_bin = 5 + (int) pVtx.z()/1.2 ;
 //     4            55-60%            30-40%
 //     5            50-55%            20-30%
